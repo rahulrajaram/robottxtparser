24a81679c549bf71cb6ee5c7e9199adb07a9585d
rahulrajaram <rahulrajaram2005@gmail.com>
2025-06-15 01:14:15 -0400

    Update robottxtparser.py
    - Throw exceptions in certain obvious cases'
    - support crawl-delay
    - Allow .,/,\, and space in useragent values
    - support sitemaps

--- robottxtparser.py
+++ robottxtparser.py
@@ -11,12 +11,17 @@ Program processes a robot.txt file to verify that it is legal according to the g
 
  startgroupline = *WS "user-agent" *WS ":" *WS product-token EOL
 
+ nonstandard-field = "crawl-delay"
+
+ nonstandard-rule = *WS nonstandard-field *WS ":" *WS value EOL
 
  rule = *WS ("allow" / "disallow") *WS ":"
        *WS (path-pattern / empty-pattern) EOL
 
  ; parser implementors: define additional lines you need (for
  ; example, Sitemaps).
+
+sitemap-rule = *WS "sitemap" *WS ":" *WS URI EOL
 
  product-token = identifier 1"*" / "*"
  path-pattern = "/" *UTF8-char-noctl ; valid URI path pattern
@@ -38,10 +43,8 @@ it:
 2. allows for either an emptyline or a straightgroupline as the first expression within a group;
 the RFC expects the first line to be a group
 3. allows digits in identifiers for user-agent names
-
-```
-User-agent: asia*
-```
+4. supports crawl-delay with floating point values
+5. supports Sitemaps nominally
 """
 import os
 import re
@@ -70,7 +73,7 @@ def is_emptyline(line):
 
 
 def is_identifier(token):
-    identifier_regex = re.compile(r'^[-a-zA-Z0-9_]+[\*]?$')
+    identifier_regex = re.compile(r'^[-a-zA-Z0-9_ ./\\]+[\*]?$')
     mtch = identifier_regex.match(token)
     return mtch and mtch.group()
 
@@ -84,19 +87,21 @@ def is_product_token(token):
 
 def is_path_pattern(token):
     """
-    Handle
-
+    Returns True if token conforms to:
     path-pattern = "/" *UTF8-char-noctl
+    Where UTF8-char-noctl excludes control characters (0x00–0x1F, 0x7F).
     """
-    if not token.startswith('/'):
+    if not token.startswith('/') and not token.startswith('*/'):
         return False
-    if len(token) == 1:
-        return True
     try:
         token.encode('utf-8')
     except UnicodeEncodeError:
         return False
-    return all('\x20' <= c <= '\x7E' or ord(c) > 127 for c in token[1:])
+    for c in token[1:]:
+        o = ord(c)
+        if o < 0x20 or o == 0x7F:
+            return False
+    return True
 
 
 def is_rule(line):
@@ -185,11 +190,69 @@ def is_startgroupline(line):
         i += 1
 
     # product-token EOL
-    if i == len(tokens) or not is_product_token(tokens[i]):
-        raise RuntimeError(
-            f"Expected identifier but found {tokens[i]}. "
-            "Identifiers follow the regex r'^[-a-zA-Z0-9_]+[\\*]{1}$'"
+    if i == len(tokens):
+        raise ValueError(
+            f"Expected identifier but found EOL at {line}."
         )
+
+    while i < len(tokens):
+        if not is_product_token(tokens[i]):
+            raise ValueError(
+                f"Expected identifier but found {tokens[i]}. "
+                "Identifiers follow the regex r'^[-a-zA-Z0-9_]+[\\*]{1}$'"
+            )
+        i += 1
+
+    return i == len(tokens)
+
+
+def is_nonstandard_rule(line):
+    tokens = [token.strip() for token in line.split() if token.strip()]
+    i = 0
+
+    # *WS
+    while i < len(tokens):
+        if not is_WS(tokens[i]):
+            break
+        i += 1
+
+    # "crawl-delay" *WS ":"
+    if i < len(tokens) and tokens[i].lower() in [
+        "crawl-delay"
+    ]:
+        i += 1
+        while i < len(tokens):
+            if not is_WS(tokens[i]):
+                break
+            i += 1
+        if i == len(tokens) or not tokens[i] != ':':
+            return False
+    elif i < len(tokens) and tokens[i].lower() in [
+        "crawl-delay:"
+    ]:
+        i += 1
+    else:
+        return False
+
+    # *WS
+    while i < len(tokens):
+        if not is_WS(tokens[i]):
+            break
+        i += 1
+
+    # product-token EOL
+    if i < len(tokens):
+        try:
+            float(tokens[i])
+        except ValueError:
+            raise ValueError(
+                f"Expected floating-point value but found {tokens[i]} at {line}."
+            )
+    else:
+        raise ValueError(
+            f"Expected floating-point value but found EOL at {line}."
+        )
+
     i += 1
     return i == len(tokens)
 
@@ -206,31 +269,63 @@ def is_group(contents, i):
     """
     if not len(contents):
         return False, i
-    i += 1
+    # i += 1
 
     # *(startgroupline / emptyline)
+    group_found = False
     while i < len(contents):
         if is_startgroupline(contents[i]):
             i += 1
-            continue
+            group_found = True
         elif is_emptyline(contents[i]):
             i += 1
-            continue
+            group_found = True
         else:
             break
+    if group_found:
+        return True, i
 
     # *(rule / emptyline)
     while i < len(contents):
         if is_rule(contents[i]):
             i += 1
-            continue
+            group_found = True
         elif is_emptyline(contents[i]):
             i += 1
-            continue
+            group_found = True
         else:
             break
+    if group_found:
+        return True, i
 
-    return True, i  
+    while i < len(contents):
+        if is_nonstandard_rule(contents[i]):
+            i += 1
+            group_found = True
+        elif is_emptyline(contents[i]):
+            i += 1
+            group_found = True
+        else:
+            break
+    if group_found:
+        return True, i
+
+    while i < len(contents):
+        if contents[i].lower().startswith('sitemap'):
+            i += 1
+            
+            group_found = True
+        elif is_emptyline(contents[i]):
+            i += 1
+            group_found = True
+        else:
+            break
+    if group_found:
+        return True, i
+
+    raise RuntimeError(
+        f"Expected to find a startgroupline, a rule, a crawl-delay, or a sitemap, but found {contents[i]}."
+    )
 
 
 def is_robotstxt(contents):
@@ -266,12 +361,12 @@ def main():
         help="Path to robots.txt file (default: ./robots.txt)"
     )
     args = parser.parse_args()
-    try:
-        result = is_valid(args.path)
-        print(result)
-    except Exception as e:
-        print(f"Error: {e}")
+    is_valid(args.path)
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except Exception as e:
+        print(f"Error: {e}")
+        exit(1)
